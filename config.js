a480c619938a8c108631e35bee75868ce36e41eb
buivanloi <100933683+buivanloi@users.noreply.github.com>
2022-03-07 17:02:20 +0700

    Add files via upload
    helu

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '8/3';
+const giftUrl = 'https://www.facebook.com/ltnhthien.28803';
+const eventName = 'Bùi Lợi chúc mừng ngày 8-3';
+const titleCard = 'Tặng cô gái';
+const contentCard = 'Chúc mày 8/3 vui vẻ, sớm khỏe để đi chơi. Từ trước đến nay tao biết tới mày là một cô gái bên ngoài mạnh mẽ bên trong sợ ma, thế nên hãy cứ là em của hiện tại dễ thương dễ mến nhé. Acsimet từng nói hãy cho ông 1 điểm tựa ông sẽ nâng cả Trái Đất, nếu có một ngày mọi thứ chống lại m thì đừng quên Bùi Lợi sẽ cho Như Thiện 1 điểm tựa để chống lại tất cả!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'thien.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
